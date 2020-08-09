6f9fb8b5bd35d324852c4681eb1746e17b273e34
Helen Zhang <759787005hyz@gmail.com>
2020-08-09 17:14:58 -0400

    Created project & main

---
+++ cmake-build-debug/CMakeFiles/C_Shell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Shell.dir/main.c.o"
+  "C_Shell"
+  "C_Shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,17 @@
+// Create by Helen Z in August 2020.
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char **argv) {
+    // Load any config files.
+
+    // Run the main command loop.
+    // - interactive or from file
+
+    // Perform any necessary kill/cleanup jobs.
+    // - execute shutdown command
+    // - free memory
+    // - terminate program
+
+    return EXIT_SUCCESS;
+}
